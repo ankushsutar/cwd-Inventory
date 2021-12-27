0947de47f563b849da33631f9b71eb0e2e6c45d9
ankushsutar <93184401+ankushsutar@users.noreply.github.com>
2021-12-27 19:05:36 +0530

    Add files via upload

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -14,7 +14,7 @@ exports.view = (req, res) => {
       pool.getConnection((err,connection) => {
      // if(err) throw err;
     console.log('connected as ID'+ connection.threadId);
-       connection.query('SELECT * FROM user', (err, rows) => {
+       connection.query('SELECT * FROM users', (err, rows) => {
          connection.release();
             if(!err) {
               res.render('home', { rows });
@@ -34,8 +34,8 @@ exports.find = (req, res) =>  {
 
       let searchTerm = req.body.search;
         console.log(searchTerm);
-          connection.query('SELECT * FROM user WHERE first_name LIKE ?', ['%'+ searchTerm + '%'], (err, rows) => {
-            connection.release();
+          connection.query('SELECT * FROM users WHERE first_name LIKE ?', ['%'+ searchTerm + '%'], (err, rows) => {
+             connection.release();
                if(!err) {
                  res.render('home', { rows });
                } else {
@@ -43,9 +43,66 @@ exports.find = (req, res) =>  {
                }
                // console.log('data from users table: \n', rows);
             });
-       });
+    });
+
+}
 
 
+exports.create = (req, res) =>  {
+ //res.render('add-user');
+  const { first_name, last_name, email, phone, status} = req.body;
+
+  pool.getConnection((err,connection) => {
+    // if(err) throw err;
+   //console.log('connected as ID'+ connection.threadId);
+      connection.query('INSERT INTO users SET first_name = ?, last_name= ?, email= ?, phone= ?, status= ?, createdAt= CURDATE()',[first_name, last_name, email, phone, status], (err, rows) => {
+         connection.release();
+           if(!err) {
+             res.render('add-user', { rows });
+           } else {
+               console.log(err);
+           }
+           // console.log('data from users table: \n', rows);
+        });
+});
 
 
 }
+
+
+
+exports.delete = (req, res) =>  {
+
+  pool.getConnection((err,connection) => {
+      // if(err) throw err;
+     //console.log('connected as ID'+ connection.threadId);
+        connection.query('DELETE FROM users WHERE id= ?', [req.params.id], (err, rows) => {
+           connection.release();
+             if(!err) {
+               res.redirect('/');
+             } else {
+                 console.log(err);
+             }
+             console.log('data from users table: \n', rows);
+          });
+  });
+
+}
+
+
+exports.form = (req, res) =>  {
+  res.render('add-user');
+ 
+}
+
+
+
+exports.dashboard = (req, res) =>  {
+  res.render('dashboard');
+ 
+ }
+
+ exports.inspection = (req, res) =>  {
+  res.render('inspection');
+ 
+ }
--- server/routes/user.js
+++ server/routes/user.js
@@ -6,6 +6,11 @@ const userController = require('../controllers/userController');
 
 router.get('/', userController.view);
 router.post('/', userController.find);
+router.get('/:id', userController.delete);
+router.get('/adduser', userController.form);
+router.post('/adduser', userController.create);
+router.get('/dashboard', userController.dashboard);
+router.get('/inspection', userController.inspection);
 //Router
 // router.get('',(req, res) =>{
 //     res.render('home');
